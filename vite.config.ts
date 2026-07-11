367da6bdad10f76f4defa0e7343bd5ef93b44298
cpjuanluismartell-lgtm <cpjuanluismartell@gmail.com>
2026-07-11 16:35:51 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/lector-de-cfdi-xml-v2/', // ← Nombre de tu repositorio
     const env = loadEnv(mode, '.', '');
+    base: '/lector-de-cfdi-xml-v2/', // ← ¡Movido aquí adentro!
     return {
       server: {
         port: 3000,
